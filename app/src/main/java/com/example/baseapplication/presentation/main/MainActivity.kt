7d0214a9c4245ba498c5cd02e1b59f6b25bdf3e6
smoralber <smoralber@gmail.com>
2021-07-14 17:11:31 +0200

    Add splash screen

--- app/src/main/java/com/example/baseapplication/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/baseapplication/presentation/main/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.baseapplication.presentation.main
 
+import android.os.Bundle
 import com.example.baseapplication.R
 import com.example.baseapplication.presentation.base.BaseActivity
 
 class MainActivity : BaseActivity(hastOptionsMenu = true) {
 
     override fun getLayoutRes() = R.layout.activity_main
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+    }
 }
