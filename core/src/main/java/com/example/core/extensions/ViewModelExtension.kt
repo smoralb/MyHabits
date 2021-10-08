1a03aae9123a96ae7ca21021bd939bf27b7c0392
smoralber <smoralber@gmail.com>
2021-10-08 19:08:00 +0200

    Update libraries

--- core/src/main/java/com/example/core/extensions/ViewModelExtension.kt
+++ core/src/main/java/com/example/core/extensions/ViewModelExtension.kt
@@ -4,6 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
+/**
+ * viewModelScope it attached to the lifecycle of the ViewModel, so this scope will be cancelled
+ * when the ViewModel is cleared
+ */
 fun ViewModel.execute(useCase: suspend () -> Unit) {
     viewModelScope.launch { useCase() }
 }
