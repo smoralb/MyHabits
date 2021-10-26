8a11488c4405af3c43ffcd46d28ec2004d0fbb41
smoralber@gmail.com <smoralber@gmail.com>
2021-10-26 15:46:27 -0400

    Increase code coverage

--- app/src/test/java/com/example/baseapplication/presentation/SecondViewModelTest.kt
+++ app/src/test/java/com/example/baseapplication/presentation/SecondViewModelTest.kt
@@ -1,10 +1,17 @@
 package com.example.baseapplication.presentation
 
+import com.example.baseapplication.presentation.main.secondView.SecondViewModel
 import com.example.core.test.BaseViewModelUnitTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.jupiter.api.BeforeEach
 
 @ExperimentalCoroutinesApi
-class SecondViewModelTest: BaseViewModelUnitTest() {
+class SecondViewModelTest : BaseViewModelUnitTest() {
 
+    private lateinit var viewModel: SecondViewModel
 
+    @BeforeEach
+    fun setUp() {
+        viewModel = SecondViewModel()
+    }
 }
