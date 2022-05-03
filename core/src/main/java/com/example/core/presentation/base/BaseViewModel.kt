bd5bacc5623cdaab42ec85ce132083174a6a6ee4
smoralber <smoralber@gmail.com>
2022-05-03 10:27:05 -0400

    Refactor code

--- core/src/main/java/com/example/core/presentation/base/BaseViewModel.kt
+++ core/src/main/java/com/example/core/presentation/base/BaseViewModel.kt
@@ -5,10 +5,8 @@ import androidx.lifecycle.ViewModel
 
 abstract class BaseViewModel<S : BaseState> : ViewModel() {
 
-<<<<<<< HEAD
     protected val _viewState: MutableLiveData<S> = MutableLiveData()
-=======
+
     val viewState: MutableLiveData<S> = MutableLiveData()
->>>>>>> e52c2b9d2aa6eabffb57c8c86b863ed0cc0939f3
 
 }
