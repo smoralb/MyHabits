6239bc658c6bdfad7675370a25feae433c63735e
smoralber <smoralber@gmail.com>
2022-06-16 14:36:05 -0400

    Added tests for repository

--- ft_auth/src/test/java/com/smb/ft_auth/data/AuthRepositoryTest.kt
+++ ft_auth/src/test/java/com/smb/ft_auth/data/AuthRepositoryTest.kt
@@ -47,4 +47,40 @@ class AuthRepositoryTest : BaseUnitTest() {
             clearInvocations(remoteSource)
         }
     }
+
+    @TestFactory
+    fun `login should return`() = listOf(
+        Result.Success(Unit), Result.Error()
+    ).map { testCase ->
+        DynamicTest.dynamicTest("$testCase") {
+            runBlockingTest {
+                whenever(remoteSource.login(EMPTY_STRING, EMPTY_STRING)).thenReturn(
+                    testCase
+                )
+
+                val result = repository.login(EMPTY_STRING, EMPTY_STRING)
+                verify(remoteSource).login(EMPTY_STRING, EMPTY_STRING)
+                assertEquals(testCase, result)
+            }
+            clearInvocations(remoteSource)
+        }
+    }
+
+    @TestFactory
+    fun `recoverPassword should return`() = listOf(
+        Result.Success(Unit), Result.Error()
+    ).map { testCase ->
+        DynamicTest.dynamicTest("$testCase") {
+            runBlockingTest {
+                whenever(remoteSource.recoverPassword(EMPTY_STRING)).thenReturn(
+                    testCase
+                )
+
+                val result = repository.recoverPassword(EMPTY_STRING)
+                verify(remoteSource).recoverPassword(EMPTY_STRING)
+                assertEquals(testCase, result)
+            }
+            clearInvocations(remoteSource)
+        }
+    }
 }
